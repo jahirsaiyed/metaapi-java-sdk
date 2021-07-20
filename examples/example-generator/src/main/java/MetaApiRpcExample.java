3fffae91f39b596e9df15e88d035f2950cc0b8d8
Aleksander M <oleksandr.m.as@agiliumlabs.com>
2021-07-20 09:01:29 +0300

    Update versions

--- examples/example-generator/src/main/java/MetaApiRpcExample.java
+++ examples/example-generator/src/main/java/MetaApiRpcExample.java
@@ -21,83 +21,83 @@ import cloud.metaapi.sdk.util.JsonMapper;
  */
 public class MetaApiRpcExample {
 
-    private static String token = getEnvOrDefault("TOKEN", "<put in your token here>");
-    private static String accountId = getEnvOrDefault("ACCOUNT_ID", "<put in your account id here>");
-    
-    public static void main(String[] args) {
-        try {
-            MetaApi api = new MetaApi(token);
-            
-            MetatraderAccount account = api.getMetatraderAccountApi().getAccount(accountId).get();
-            DeploymentState initialState = account.getState();
-            List<DeploymentState> deployedStates = new ArrayList<>();
-            deployedStates.add(DeploymentState.DEPLOYING);
-            deployedStates.add(DeploymentState.DEPLOYED);
-            
-            if (!deployedStates.contains(initialState)) {
-                // wait until account is deployed and connected to broker
-                System.out.println("Deploying account");
-                account.deploy().get();
-            }
-            
-            System.out.println("Waiting for API server to connect to broker (may take couple of minutes)");
-            account.waitConnected().get();
-            
-            // connect to MetaApi API
-            MetaApiConnection connection = account.connect().get();
-            
-            System.out.println("Waiting for SDK to synchronize to terminal state "
-                + "(may take some time depending on your history size)");
-            connection.waitSynchronized().get();
-            
-            // invoke RPC API (replace ticket numbers with actual ticket numbers which exist in your MT account)
-            System.out.println("Testing MetaAPI RPC API");
-            System.out.println("account information: " + asJson(connection.getAccountInformation().get()));
-            System.out.println("positions: " + asJson(connection.getPositions().get()));
-            System.out.println("open orders:" + asJson(connection.getOrders().get()));
-            System.out.println("history orders by ticket: " + asJson(connection.getHistoryOrdersByTicket("1234567").get()));
-            System.out.println("history orders by position: " + asJson(connection.getHistoryOrdersByPosition("1234567").get()));
-            System.out.println("history orders (~last 3 months): " + asJson(connection.getHistoryOrdersByTimeRange(
-                new IsoTime(Date.from(Instant.now().plusSeconds(-90 * 24 * 60 * 60))), 
-                new IsoTime(Date.from(Instant.now())),
-                0, 1000).get()));
-            System.out.println("history deals by ticket: " + asJson(connection.getDealsByTicket("1234567").get()));
-            System.out.println("history deals by position: " + asJson(connection.getDealsByPosition("1234567").get()));
-            System.out.println("history deals (~last 3 months): " + asJson(connection.getDealsByTimeRange(
-                new IsoTime(Date.from(Instant.now().plusSeconds(-90 * 24 * 60 * 60))), 
-                new IsoTime(Date.from(Instant.now())), 
-                0, 1000).get()));
-            
-            // trade
-            System.out.println("Submitting pending order");
-            try {
-                MetatraderTradeResponse result = connection
-                    .createLimitBuyOrder("GBPUSD", 0.07, 1.0, 0.9, 2.0, new PendingTradeOptions() {{
-                        comment = "comm"; clientId = "TE_GBPUSD_7hyINWqAlE"; 
-                    }}).get();
-                System.out.println("Trade successful, result code is " + result.stringCode);
-            } catch (ExecutionException err) {
-                System.out.println("Trade failed with result code " + ((TradeException) err.getCause()).stringCode);
-            }
-            
-            if (!deployedStates.contains(initialState)) {
-                // finally, undeploy account
-                System.out.println("Undeploying account so that it does not consume any unwanted resources");
-                account.undeploy().get();
-            }
-            
-        } catch (Exception err) {
-            System.err.println(err);
-        }
-        System.exit(0);
+  private static String token = getEnvOrDefault("TOKEN", "<put in your token here>");
+  private static String accountId = getEnvOrDefault("ACCOUNT_ID", "<put in your account id here>");
+  
+  public static void main(String[] args) {
+    try {
+      MetaApi api = new MetaApi(token);
+
+      MetatraderAccount account = api.getMetatraderAccountApi().getAccount(accountId).get();
+      DeploymentState initialState = account.getState();
+      List<DeploymentState> deployedStates = new ArrayList<>();
+      deployedStates.add(DeploymentState.DEPLOYING);
+      deployedStates.add(DeploymentState.DEPLOYED);
+      
+      if (!deployedStates.contains(initialState)) {
+        // wait until account is deployed and connected to broker
+        System.out.println("Deploying account");
+        account.deploy().get();
+      }
+      
+      System.out.println("Waiting for API server to connect to broker (may take couple of minutes)");
+      account.waitConnected().get();
+      
+      // connect to MetaApi API
+      MetaApiConnection connection = account.connect().get();
+      
+      System.out.println("Waiting for SDK to synchronize to terminal state "
+        + "(may take some time depending on your history size)");
+      connection.waitSynchronized().get();
+      
+      // invoke RPC API (replace ticket numbers with actual ticket numbers which exist in your MT account)
+      System.out.println("Testing MetaAPI RPC API");
+      System.out.println("account information: " + asJson(connection.getAccountInformation().get()));
+      System.out.println("positions: " + asJson(connection.getPositions().get()));
+      System.out.println("open orders:" + asJson(connection.getOrders().get()));
+      System.out.println("history orders by ticket: " + asJson(connection.getHistoryOrdersByTicket("1234567").get()));
+      System.out.println("history orders by position: " + asJson(connection.getHistoryOrdersByPosition("1234567").get()));
+      System.out.println("history orders (~last 3 months): " + asJson(connection.getHistoryOrdersByTimeRange(
+        new IsoTime(Date.from(Instant.now().plusSeconds(-90 * 24 * 60 * 60))), 
+        new IsoTime(Date.from(Instant.now())),
+        0, 1000).get()));
+      System.out.println("history deals by ticket: " + asJson(connection.getDealsByTicket("1234567").get()));
+      System.out.println("history deals by position: " + asJson(connection.getDealsByPosition("1234567").get()));
+      System.out.println("history deals (~last 3 months): " + asJson(connection.getDealsByTimeRange(
+        new IsoTime(Date.from(Instant.now().plusSeconds(-90 * 24 * 60 * 60))), 
+        new IsoTime(Date.from(Instant.now())), 
+        0, 1000).get()));
+      
+      // trade
+      System.out.println("Submitting pending order");
+      try {
+        MetatraderTradeResponse result = connection
+          .createLimitBuyOrder("GBPUSD", 0.07, 1.0, 0.9, 2.0, new PendingTradeOptions() {{
+            comment = "comm"; clientId = "TE_GBPUSD_7hyINWqAlE"; 
+          }}).get();
+        System.out.println("Trade successful, result code is " + result.stringCode);
+      } catch (ExecutionException err) {
+        System.out.println("Trade failed with result code " + ((TradeException) err.getCause()).stringCode);
+      }
+      
+      if (!deployedStates.contains(initialState)) {
+        // finally, undeploy account
+        System.out.println("Undeploying account so that it does not consume any unwanted resources");
+        account.undeploy().get();
+      }
+      
+    } catch (Exception err) {
+      System.err.println(err);
     }
-    
-    private static String getEnvOrDefault(String name, String defaultValue) {
-        String result = System.getenv(name);
-        return (result != null ? result : defaultValue);
-    }
-    
-    private static String asJson(Object object) throws JsonProcessingException {
-        return JsonMapper.getInstance().writeValueAsString(object);
-    }
+    System.exit(0);
+  }
+  
+  private static String getEnvOrDefault(String name, String defaultValue) {
+    String result = System.getenv(name);
+    return (result != null ? result : defaultValue);
+  }
+  
+  private static String asJson(Object object) throws JsonProcessingException {
+    return JsonMapper.getInstance().writeValueAsString(object);
+  }
 }
--- examples/mt4/src/main/java/MetaApiSynchronizationExample.java
+++ examples/mt4/src/main/java/MetaApiSynchronizationExample.java
@@ -21,115 +21,115 @@ import cloud.metaapi.sdk.util.JsonMapper;
  */
 public class MetaApiSynchronizationExample {
 
-    private static String token = getEnvOrDefault("TOKEN", "<put in your token here>");
-    private static String login = getEnvOrDefault("LOGIN", "<put in your MT login here>");
-    private static String password = getEnvOrDefault("PASSWORD", "<put in your MT password here>");
-    private static String serverName = getEnvOrDefault("SERVER", "<put in your MT server name here>");
-    private static String brokerSrvFile = getEnvOrDefault("PATH_TO_BROKER_SRV", "/path/to/your/broker.srv");
-    
-    public static void main(String[] args) {
-        try {
-            MetaApi api = new MetaApi(token);
-            List<ProvisioningProfile> profiles = api.getProvisioningProfileApi().getProvisioningProfiles().get();
-            
-            // create test MetaTrader account profile
-            Optional<ProvisioningProfile> profile = profiles.stream()
-                .filter(p -> p.getName().equals(serverName))
-                .findFirst();
-            if (!profile.isPresent()) {
-                System.out.println("Creating account profile");
-                NewProvisioningProfileDto newDto = new NewProvisioningProfileDto() {{
-                    name = serverName;
-                    version = 4;
-                    brokerTimezone = "EET";
-                    brokerDSTSwitchTimezone = "EET";
-                }};
-                profile = Optional.of(api.getProvisioningProfileApi().createProvisioningProfile(newDto).get());
-                profile.get().uploadFile("broker.srv", brokerSrvFile).get();
-            }
-            if (profile.isPresent() && profile.get().getStatus().equals("new")) {
-                System.out.println("Uploading broker.srv");
-                profile.get().uploadFile("broker.srv", brokerSrvFile).get();
-            } else {
-                System.out.println("Account profile already created");
-            }
-            
-            // Add test MetaTrader account
-            List<MetatraderAccount> accounts = api.getMetatraderAccountApi().getAccounts().get();
-            Optional<MetatraderAccount> account = accounts.stream()
-                .filter(a -> a.getLogin().equals(login) && a.getType().startsWith("cloud"))
-                .findFirst();
-            if (!account.isPresent()) {
-                System.out.println("Adding MT4 account to MetaApi");
-                String mtLogin = login;
-                String mtPassword = password;
-                ProvisioningProfile provisioningProfile = profile.get();
-                account = Optional.of(api.getMetatraderAccountApi().createAccount(new NewMetatraderAccountDto() {{
-                    name = "Test account";
-                    type = "cloud";
-                    login = mtLogin;
-                    password = mtPassword;
-                    server = serverName;
-                    provisioningProfileId = provisioningProfile.getId();
-                    application = "MetaApi";
-                    magic = 1000;
-                }}).get());
-            } else {
-                System.out.println("MT4 account already added to MetaApi");
-            }
-            
-            // wait until account is deployed and connected to broker
-            System.out.println("Deploying account");
-            account.get().deploy().get();
-            System.out.println("Waiting for API server to connect to broker (may take couple of minutes)");
-            account.get().waitConnected().get();
-            
-            // connect to MetaApi API
-            MetaApiConnection connection = account.get().connect().get();
-            
-            System.out.println("Waiting for SDK to synchronize to terminal state "
-                + "(may take some time depending on your history size)");
-            connection.waitSynchronized().get();
+  private static String token = getEnvOrDefault("TOKEN", "<put in your token here>");
+  private static String login = getEnvOrDefault("LOGIN", "<put in your MT login here>");
+  private static String password = getEnvOrDefault("PASSWORD", "<put in your MT password here>");
+  private static String serverName = getEnvOrDefault("SERVER", "<put in your MT server name here>");
+  private static String brokerSrvFile = getEnvOrDefault("PATH_TO_BROKER_SRV", "/path/to/your/broker.srv");
+  
+  public static void main(String[] args) {
+    try {
+      MetaApi api = new MetaApi(token);
+      List<ProvisioningProfile> profiles = api.getProvisioningProfileApi().getProvisioningProfiles().get();
+      
+      // create test MetaTrader account profile
+      Optional<ProvisioningProfile> profile = profiles.stream()
+        .filter(p -> p.getName().equals(serverName))
+        .findFirst();
+      if (!profile.isPresent()) {
+        System.out.println("Creating account profile");
+        NewProvisioningProfileDto newDto = new NewProvisioningProfileDto() {{
+          name = serverName;
+          version = 4;
+          brokerTimezone = "EET";
+          brokerDSTSwitchTimezone = "EET";
+        }};
+        profile = Optional.of(api.getProvisioningProfileApi().createProvisioningProfile(newDto).get());
+        profile.get().uploadFile("broker.srv", brokerSrvFile).get();
+      }
+      if (profile.isPresent() && profile.get().getStatus().equals("new")) {
+        System.out.println("Uploading broker.srv");
+        profile.get().uploadFile("broker.srv", brokerSrvFile).get();
+      } else {
+        System.out.println("Account profile already created");
+      }
+      
+      // Add test MetaTrader account
+      List<MetatraderAccount> accounts = api.getMetatraderAccountApi().getAccounts().get();
+      Optional<MetatraderAccount> account = accounts.stream()
+        .filter(a -> a.getLogin().equals(login) && a.getType().startsWith("cloud"))
+        .findFirst();
+      if (!account.isPresent()) {
+        System.out.println("Adding MT4 account to MetaApi");
+        String mtLogin = login;
+        String mtPassword = password;
+        ProvisioningProfile provisioningProfile = profile.get();
+        account = Optional.of(api.getMetatraderAccountApi().createAccount(new NewMetatraderAccountDto() {{
+          name = "Test account";
+          type = "cloud";
+          login = mtLogin;
+          password = mtPassword;
+          server = serverName;
+          provisioningProfileId = provisioningProfile.getId();
+          application = "MetaApi";
+          magic = 1000;
+        }}).get());
+      } else {
+        System.out.println("MT4 account already added to MetaApi");
+      }
+      
+      // wait until account is deployed and connected to broker
+      System.out.println("Deploying account");
+      account.get().deploy().get();
+      System.out.println("Waiting for API server to connect to broker (may take couple of minutes)");
+      account.get().waitConnected().get();
+      
+      // connect to MetaApi API
+      MetaApiConnection connection = account.get().connect().get();
+      
+      System.out.println("Waiting for SDK to synchronize to terminal state "
+        + "(may take some time depending on your history size)");
+      connection.waitSynchronized().get();
 
-            // access local copy of terminal state
-            System.out.println("Testing terminal state access");
-            TerminalState terminalState = connection.getTerminalState();
-            System.out.println("connected: " + terminalState.isConnected());
-            System.out.println("connected to broker: " + terminalState.isConnectedToBroker());
-            System.out.println("account information: " + asJson(terminalState.getAccountInformation().orElse(null)));
-            System.out.println("positions: " + asJson(terminalState.getPositions()));
-            System.out.println("orders: " + asJson(terminalState.getOrders()));
-            System.out.println("specifications: " + asJson(terminalState.getSpecifications()));
-            System.out.println("EURUSD specification: " + asJson(terminalState.getSpecification("EURUSD").orElse(null)));
-            System.out.println("EURUSD price: " + asJson(terminalState.getPrice("EURUSD").orElse(null)));
-            
-            // trade
-            System.out.println("Submitting pending order");
-            try {
-                MetatraderTradeResponse result = connection
-                    .createLimitBuyOrder("GBPUSD", 0.07, 1.0, 0.9, 2.0, new PendingTradeOptions() {{
-                        comment = "comm"; clientId = "TE_GBPUSD_7hyINWqAlE"; 
-                    }}).get();
-                System.out.println("Trade successful, result code is " + result.stringCode);
-            } catch (ExecutionException err) {
-                System.out.println("Trade failed with result code " + ((TradeException) err.getCause()).stringCode);
-            }
-            
-            // finally, undeploy account
-            System.out.println("Undeploying MT4 account so that it does not consume any unwanted resources");
-            account.get().undeploy().get();
-        } catch (Exception err) {
-            System.err.println(err);
-        }
-        System.exit(0);
+      // access local copy of terminal state
+      System.out.println("Testing terminal state access");
+      TerminalState terminalState = connection.getTerminalState();
+      System.out.println("connected: " + terminalState.isConnected());
+      System.out.println("connected to broker: " + terminalState.isConnectedToBroker());
+      System.out.println("account information: " + asJson(terminalState.getAccountInformation().orElse(null)));
+      System.out.println("positions: " + asJson(terminalState.getPositions()));
+      System.out.println("orders: " + asJson(terminalState.getOrders()));
+      System.out.println("specifications: " + asJson(terminalState.getSpecifications()));
+      System.out.println("EURUSD specification: " + asJson(terminalState.getSpecification("EURUSD").orElse(null)));
+      System.out.println("EURUSD price: " + asJson(terminalState.getPrice("EURUSD").orElse(null)));
+      
+      // trade
+      System.out.println("Submitting pending order");
+      try {
+        MetatraderTradeResponse result = connection
+          .createLimitBuyOrder("GBPUSD", 0.07, 1.0, 0.9, 2.0, new PendingTradeOptions() {{
+            comment = "comm"; clientId = "TE_GBPUSD_7hyINWqAlE"; 
+          }}).get();
+        System.out.println("Trade successful, result code is " + result.stringCode);
+      } catch (ExecutionException err) {
+        System.out.println("Trade failed with result code " + ((TradeException) err.getCause()).stringCode);
+      }
+      
+      // finally, undeploy account
+      System.out.println("Undeploying MT4 account so that it does not consume any unwanted resources");
+      account.get().undeploy().get();
+    } catch (Exception err) {
+      System.err.println(err);
     }
-    
-    private static String getEnvOrDefault(String name, String defaultValue) {
-        String result = System.getenv(name);
-        return (result != null ? result : defaultValue);
-    }
-    
-    private static String asJson(Object object) throws JsonProcessingException {
-        return JsonMapper.getInstance().writeValueAsString(object);
-    }
+    System.exit(0);
+  }
+  
+  private static String getEnvOrDefault(String name, String defaultValue) {
+    String result = System.getenv(name);
+    return (result != null ? result : defaultValue);
+  }
+  
+  private static String asJson(Object object) throws JsonProcessingException {
+    return JsonMapper.getInstance().writeValueAsString(object);
+  }
 }
